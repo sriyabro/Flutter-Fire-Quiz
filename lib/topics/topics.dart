e04160319cda7fbd48a38e25801a220816e86d68
sriyabro <sriyabro@gmail.com>
2022-09-04 01:04:04 +0530

    topics screen init changes

--- lib/topics/topics.dart
+++ lib/topics/topics.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizapp/services/firestore.dart';
+import 'package:quizapp/services/models.dart';
 import 'package:quizapp/shared/bottom_nav.dart';
 
 class TopicsScreen extends StatelessWidget {
@@ -6,8 +8,33 @@ class TopicsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavBar(),
+    return FutureBuilder<List<Topic>>(
+      future: FirestoreService().getTopics(),
+      builder: ((context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: Text('loading...'));
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Error: ${snapshot.error}'));
+        } else if (snapshot.hasData) {
+          var topics = snapshot.data!;
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Topics'),
+            ),
+            body: GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: topics.map((topic) => Text(topic.title)).toList(),
+            ),
+            bottomNavigationBar: const BottomNavBar(),
+          );
+        } else {
+          // snapshot has no error and no data
+          return const Center(child: Text('No topics here yet'));
+        }
+      }),
     );
   }
 }
