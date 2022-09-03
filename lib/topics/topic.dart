335f3c0a9fa2b886fb69c5d48869604d6b9fa639
sriyabro <sriyabro@gmail.com>
2022-09-04 01:44:26 +0530

    topic screen changes

---
+++ lib/topics/topic.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/services/models.dart';
+
+class TopicScreen extends StatelessWidget {
+  final Topic topic;
+  const TopicScreen({Key? key, required this.topic}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+      ),
+      body: ListView(
+        children: [
+          Hero(
+            tag: topic.img,
+            child: Image.asset(
+              'assets/covers/${topic.img}',
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          Text(
+            topic.title,
+            style: const TextStyle(height: 2, fontSize: 20, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
+  }
+}
