2420b2d7b596b4d66a5da918cf9515b36c57524a
sriyabro <sriyabro@gmail.com>
2022-09-05 00:55:30 +0530

    topics drawer added

---
+++ lib/topics/drawer.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/services/models.dart';
+
+class TopicDrawer extends StatelessWidget {
+  final List<Topic> topics;
+  const TopicDrawer({Key? key, required this.topics}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView.separated(
+        itemBuilder: (BuildContext context, int index) {
+          Topic topic = topics[index];
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 10, left: 10),
+                child: Text(
+                  topic.title,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white70,
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemCount: topics.length,
+        shrinkWrap: true,
+      ),
+    );
+  }
+}
